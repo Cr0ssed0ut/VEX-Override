60d81d2288373b23fa113c22351b447179543a02
Ethan Lin <ethanlin419@gmail.com>
2026-09-16 21:45:02 -0600

    First Commit

---
+++ include/lemlib/configs.h
@@ -0,0 +1,16 @@
+#ifndef _PROS_CONFIGS_H_
+#define _PROS_CONFIGS_H_
+
+#include "api.h"
+#include "lemlib/api.hpp"
+#include "main.h"
+
+extern pros::MotorGroup left_motor_group;
+extern pros::MotorGroup right_motor_group;
+extern lemlib::Chassis chassis;
+
+extern pros::Motor intake;
+extern pros::Motor lift;
+
+
+#endif 
