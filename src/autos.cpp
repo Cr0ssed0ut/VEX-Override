96ed3f46ec1daf6d4e7412c3c4a674bf228b0456
Ethan Lin <ethanlin419@gmail.com>
2026-09-24 16:06:05 -0600

    Add test_autonomous function and integrate into autonomous mode

--- src/autos.cpp
+++ src/autos.cpp
@@ -2,6 +2,6 @@
 #include "lemlib/configs.h"
 #include "pros/motor_group.hpp"
 
-void autonomous() {
+void test_autonomous() {
     chassis.moveToPoint(0, 5, 100);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "lemlib/configs.h"
 #include "pros/motor_group.hpp"
+#include "lemlib/autos.h"
 
 /**
  * A callback function for LLEMU's center button.
@@ -71,7 +72,9 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+    test_autonomous();
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
